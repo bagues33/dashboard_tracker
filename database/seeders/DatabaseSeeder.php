deea642af866425074059ba2d40cdecc29ea1f0c
bagues33 <bagustofan@yahoo.com>
2026-04-02 17:42:16 +0700

    refactor: update user and permission creation methods in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Permission;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,28 +11,23 @@ class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // Default Admin User
-        User::firstOrCreate([
-            'username' => 'admin',
-            'email' => 'admin@admin.com',
-            'password' => bcrypt('password'),
-            'role' => 'admin'
-        ]);
+        User::updateOrCreate(
+            ['username' => 'admin'],
+            [
+                'email' => 'admin@admin.com',
+                'password' => bcrypt('password'),
+                'role' => 'admin',
+            ]
+        );
 
-        // Default Permissions
         $permissions = [
-            // Admin Permissions
             ['role' => 'admin', 'permission_key' => 'manage_users', 'is_enabled' => true],
             ['role' => 'admin', 'permission_key' => 'manage_boards', 'is_enabled' => true],
             ['role' => 'admin', 'permission_key' => 'view_analytics', 'is_enabled' => true],
             ['role' => 'admin', 'permission_key' => 'import_data', 'is_enabled' => true],
-            
-            // User Permissions
+
             ['role' => 'user', 'permission_key' => 'manage_users', 'is_enabled' => false],
             ['role' => 'user', 'permission_key' => 'manage_boards', 'is_enabled' => true],
             ['role' => 'user', 'permission_key' => 'view_analytics', 'is_enabled' => false],
@@ -39,7 +35,15 @@ class DatabaseSeeder extends Seeder
         ];
 
         foreach ($permissions as $permission) {
-            \App\Models\Permission::create($permission);
+            Permission::updateOrCreate(
+                [
+                    'role' => $permission['role'],
+                    'permission_key' => $permission['permission_key'],
+                ],
+                [
+                    'is_enabled' => $permission['is_enabled'],
+                ]
+            );
         }
     }
 }
